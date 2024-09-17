354b6fec65ed055471d4935a152fc06f8bac821c
sinopaaalll <nrizqullah825@gmail.com>
2024-09-17 18:22:40 +0700

    second commit

--- application/views/pages/user/kontak.php
+++ application/views/pages/user/kontak.php
@@ -21,74 +21,58 @@
     <!-- Kontak -->
     <section class="bg0 p-t-104 p-b-116">
         <div class="container">
-            <div class="flex-w flex-tr">
-                <div class="size-210 bor10 p-lr-70 p-t-55 p-b-70 p-lr-15-lg w-full-md">
-                    <form>
-                        <h4 class="mtext-105 cl2 txt-center p-b-30">
-                            Send Us A Message
-                        </h4>
 
-                        <div class="bor8 m-b-20 how-pos4-parent">
-                            <input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" name="email" placeholder="Your Email Address">
-                            <img class="how-pos4 pointer-none" src="assets/user/images/icons/icon-email.png" alt="ICON">
-                        </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="flex-w w-full p-b-42">
+                                <span class="fs-18 cl5 txt-center size-211">
+                                    <span class="lnr lnr-map-marker"></span>
+                                </span>
 
-                        <div class="bor8 m-b-30">
-                            <textarea class="stext-111 cl2 plh3 size-120 p-lr-28 p-tb-25" name="msg" placeholder="How Can We Help?"></textarea>
-                        </div>
+                                <div class="size-212 p-t-2">
+                                    <span class="mtext-110 cl2">
+                                        Alamat
+                                    </span>
 
-                        <button class="flex-c-m stext-101 cl0 size-121 bg3 bor1 hov-btn3 p-lr-15 trans-04 pointer">
-                            Submit
-                        </button>
-                    </form>
-                </div>
+                                    <p class="stext-115 cl6 size-213 p-t-18">
+                                        <?= $kontak->address ?>
+                                    </p>
+                                </div>
+                            </div>
 
-                <div class="size-210 bor10 flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
-                    <div class="flex-w w-full p-b-42">
-                        <span class="fs-18 cl5 txt-center size-211">
-                            <span class="lnr lnr-map-marker"></span>
-                        </span>
+                            <div class="flex-w w-full p-b-42">
+                                <span class="fs-18 cl5 txt-center size-211">
+                                    <span class="lnr lnr-phone-handset"></span>
+                                </span>
 
-                        <div class="size-212 p-t-2">
-                            <span class="mtext-110 cl2">
-                                Alamat
-                            </span>
+                                <div class="size-212 p-t-2">
+                                    <span class="mtext-110 cl2">
+                                        Telp
+                                    </span>
 
-                            <p class="stext-115 cl6 size-213 p-t-18">
-                                <?= $kontak->address ?>
-                            </p>
-                        </div>
-                    </div>
+                                    <p class="stext-115 cl1 size-213 p-t-18">
+                                        <a href="">+<?= $kontak->telp ?></a>
+                                    </p>
+                                </div>
+                            </div>
 
-                    <div class="flex-w w-full p-b-42">
-                        <span class="fs-18 cl5 txt-center size-211">
-                            <span class="lnr lnr-phone-handset"></span>
-                        </span>
+                            <div class="flex-w w-full">
+                                <span class="fs-18 cl5 txt-center size-211">
+                                    <span class="lnr lnr-envelope"></span>
+                                </span>
 
-                        <div class="size-212 p-t-2">
-                            <span class="mtext-110 cl2">
-                                Telp
-                            </span>
+                                <div class="size-212 p-t-2">
+                                    <span class="mtext-110 cl2">
+                                        Email
+                                    </span>
 
-                            <p class="stext-115 cl1 size-213 p-t-18">
-                                <a href="">+<?= $kontak->telp ?></a>
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="flex-w w-full">
-                        <span class="fs-18 cl5 txt-center size-211">
-                            <span class="lnr lnr-envelope"></span>
-                        </span>
-
-                        <div class="size-212 p-t-2">
-                            <span class="mtext-110 cl2">
-                                Email
-                            </span>
-
-                            <p class="stext-115 cl1 size-213 p-t-18">
-                                <?= $kontak->email ?>
-                            </p>
+                                    <p class="stext-115 cl1 size-213 p-t-18">
+                                        <?= $kontak->email ?>
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
